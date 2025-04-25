f870682241be1b9b7aea63e1f5c7d70e8bd16bbd
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 23:04:16 -0400

    Add files via upload
     Suma números ingresados hasta que la suma sea mayor o igual a 100.

---
+++ Ejercicio.34/Ejer.34/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ejer._34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿Cuántos números desea ingresar? ");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            
+            int[] numeros = new int[cantidad];
+            int suma = 0;
+
+           
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.Write($"Ingrese el número {i + 1}: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+                suma += numeros[i];
+
+                
+                if (suma >= 100)
+                {
+                    Console.WriteLine($"La sumatoria es mayor a 100, El resultado total es: {suma}");
+                    return;  
+                }
+            }
+
+            
+            Console.WriteLine($"La sumatoria total de los números es: {suma}");
+        
+        }
+    }
+}
