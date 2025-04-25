d6b8dcb0c28c95fadd5e4f940e988cb621db01e4
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 22:39:59 -0400

    Add files via upload
    Muestra el total, el mayor y menor inventario en cada almacén utilizando un arreglo bidimensional.

---
+++ Ejercicio.32/Ejer.32/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Ejer._32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] inventario = {
+            {50, 40, 90},  
+            {30, 60, 20}, 
+            {20, 70, 10}   
+        };
+
+            for (int j = 0; j < inventario.GetLength(1); j++)
+            {
+                int total = 0;
+                int mayor = int.MinValue;
+                int menor = int.MaxValue;
+                int tipoMayor = -1;
+                int tipoMenor = -1;
+
+                for (int i = 0; i < inventario.GetLength(0); i++)
+                {
+                    total += inventario[i, j];
+
+                    if (inventario[i, j] > mayor)
+                    {
+                        mayor = inventario[i, j];
+                        tipoMayor = i;
+                    }
+
+                    if (inventario[i, j] < menor)
+                    {
+                        menor = inventario[i, j];
+                        tipoMenor = i;
+                    }
+                }
+
+                Console.WriteLine($"Almacén {j + 1}");
+                Console.WriteLine($"Total de dispositivos: {total}");
+                Console.WriteLine($"Mayor cantidad de dispositivos en Almacén {j + 1}: {mayor} (Tipo {tipoMayor + 1})");
+                Console.WriteLine($"Menor cantidad de dispositivos en Almacén {j + 1}: {menor} (Tipo {tipoMenor + 1})");
+                Console.WriteLine();
+            }
+        }
+    }
+}
