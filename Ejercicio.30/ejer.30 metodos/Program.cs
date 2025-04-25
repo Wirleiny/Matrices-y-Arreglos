01b560f8d78b93b65ea3df7c621b5125b09851ab
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 22:35:32 -0400

    Add files via upload
    Realiza la suma y multiplicación de dos números utilizando métodos.

---
+++ Ejercicio.30/ejer.30 metodos/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ejer._30_metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String suma;
+            String multiplicacion; 
+
+
+            Console.WriteLine("ingrese el primer numero");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ingrese el segundo numero");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("La suma de sus numeros es: ");
+
+            static int (int numero1, int numero2);
+
+             suma = numero1 + numero2; 
+
+            return sumaMAT;  
+
+        }
+
+    }
+        
+}
