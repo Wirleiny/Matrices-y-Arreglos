344383e547b5ebb171bf6fb46fb9b0fb410f5581
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 23:08:06 -0400

    Add files via upload
     El programa define una clase base Operación con un método virtual Realizar Operación. Las clases derivadas Suma y Resta sobrescriben este método para realizar operaciones específicas. El usuario selecciona entre suma o resta y el programa realiza la operación elegida utilizando polimorfismo.

---
+++ Ejercicio.39/Ejer.39/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Ejer._39
+{
+    internal class Program
+    {
+           class Operacion
+        {
+            public virtual void RealizarOperacion()
+            {
+                Console.WriteLine("Operación básica.");
+            }
+        
+
+        class Suma : Operacion
+        {
+            public override void RealizarOperacion()
+            {
+                Console.WriteLine("Operación de Suma.");
+                Console.Write("Ingrese el primer número: ");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el segundo número: ");
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Resultado de la suma: {num1 + num2}");
+            }
+        }
+
+      
+        class Resta : Operacion
+        {
+            public override void RealizarOperacion()
+            {
+                Console.WriteLine("Operación de Resta.");
+                Console.Write("Ingrese el primer número: ");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el segundo número: ");
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Resultado de la resta: {num1 - num2}");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Operacion operacion;
+
+               
+                Console.WriteLine("Seleccione la operación (1: Suma, 2: Resta): ");
+                int opcion = int.Parse(Console.ReadLine());
+
+                if (opcion == 1)
+                    operacion = new Suma();
+                else
+                    operacion = new Resta();
+
+                
+                operacion.RealizarOperacion();
+
+                Console.ReadLine();
+            }
+        }
+     }
+  }
+
+}
+
