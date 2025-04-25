d5a14fdfdebef87c69ace8c347e47873d6db8b7c
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 23:06:06 -0400

    Add files via upload
    Utiliza arreglos para almacenar objetos y permite seleccionar entre perros y gatos para obtener sus sonidos.

---
+++ Ejercicio.36/ejer.36/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ejer._36
+{
+    internal class Program
+    {
+        class Animal
+        {
+            public virtual void HacerSonido()
+            {
+                Console.WriteLine("Este animal hace un sonido.");
+            }
+        }
+        class Perro : Animal
+        {
+            public override void HacerSonido()
+            {
+                Console.WriteLine("El perro hace: ¡Guau! ¡Guau!");
+            }
+        }
+
+
+        class Gato : Animal
+        {
+            public override void HacerSonido()
+            {
+                Console.WriteLine("El gato hace: ¡Miau! ¡Miau!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           
+            Animal miPerro = new Perro();
+            Animal miGato = new Gato();
+
+           
+            Console.WriteLine("¿De qué animal quieres saber el sonido? (perro/gato): ");
+            string animal = Console.ReadLine().ToLower();
+
+            
+            if (animal == "perro")
+            {
+                miPerro.HacerSonido();
+            }
+            else if (animal == "gato")
+            {
+                miGato.HacerSonido();
+            }
+            else
+            {
+                Console.WriteLine("Error, Vuelve a intentarlo bb");
+
+
+            }
+        }
+    }
+}
