caa8ec50fbdd53278be065870437e0abdbf5c19d
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-04-24 22:59:37 -0400

    Add files via upload
    Almacena nombres y calificaciones de estudiantes en arreglos y los muestra utilizando un método.

---
+++ Ejercicio.33/Ejer.33/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Ejer._33
+{
+    internal class Program
+    {
+        static void MostrarEstudiantes(string[] nombres, int[] calificaciones)
+        {
+            Console.WriteLine("Resultados:\n");
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine($"Nombre: {nombres[i]}");
+                Console.WriteLine($"Calificación: {calificaciones[i]}\n"); 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+            string[] nombres = new string[3];
+            int[] calificaciones = new int[3];
+
+            
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Ingrese el nombre del estudiante {i + 1}: ");
+                nombres[i] = Console.ReadLine();
+
+                Console.Write($"Ingrese la calificación de {nombres[i]}: ");
+                calificaciones[i] = int.Parse(Console.ReadLine());
+
+               
+                if (i < 2)
+                {
+                    Console.WriteLine();
+                }
+            }
+
+            
+            Console.Clear(); 
+            MostrarEstudiantes(nombres, calificaciones);
+        }
+    }
+}
